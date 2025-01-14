74dd1c383f6ff655ec2cc96cc55f14aba30b3c15
Cnernc <renuccip@icloud.com>
2025-01-14 09:34:14 +0100

    Small modification commit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="tsmodel",
+    name="tskit_learn",
     version="0.1.0",
     author="Pierre Renucci",
     description="A package for time series models with scikit-learn compatibility",
