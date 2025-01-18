aff7014acb8dbde2d41ed923908e6f8de0127c52
Cnernc <renuccip@icloud.com>
2025-01-18 23:53:50 +0100

    fit multidim

--- tskit_learn/timeseriesmodel.py
+++ tskit_learn/timeseriesmodel.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
 import multiprocessing as mp
-from typing import Generator, Tuple
 from sklearn.base import BaseEstimator
-from .utilitaires import _custom_clone_model, _clean_and_reindex
+from .utilitaires import (
+    _custom_clone_model, _fit_predict_ndarray, _fit_predict_ds,  _fit_predict_df
+)
 
 class BaseTimeSeriesModel:
 
@@ -16,7 +17,9 @@ class BaseTimeSeriesModel:
     ) -> None:
         
         self.model = model
-        assert rolling_window_size is None or rolling_window_size > lookahead_steps, ("rolling_window_size should be greater than lookahead_steps")
+        assert rolling_window_size is None or rolling_window_size > lookahead_steps, (
+            "rolling_window_size should be greater than lookahead_steps"
+            )
         self.window_params = {
             "rolling_window_size": rolling_window_size,
             "freq_retraining": freq_retraining,
@@ -25,121 +28,40 @@ class BaseTimeSeriesModel:
         }
         
         if not isinstance(model,  BaseEstimator):
-            assert hasattr(model, "fit") and hasattr(model, "predict") and hasattr(model, "get_params"), "model should have fit and predict methods"
+            assert hasattr(model, "fit") and hasattr(model, "predict"), (
+                "model should have fit and predict methods"
+            )
             Warning(f'TimeSeriesModel is optimised for sklearn.base.BaseEstimator not {model.__class__.__name__}')
         
         if not hasattr(model, "get_params"):
             Warning(f"model can't be cloned: {model.__class__.__name__}, multiprocessing won't be used. You can force the use of multiprocessing by setting n_jobs")
             BaseTimeSeriesModel.n_jobs = 1
-
-    @staticmethod
-    def window_grouper(
-        X: np.ndarray, rolling_window_size: int, freq_retraining: int, min_train_steps: int, lookahead_steps:int
-    ) -> Generator[Tuple[np.ndarray, np.ndarray], None, None]:
-        
-        assert 2 < min_train_steps <= len(X), ("min_train_steps should be less than or equal to the length of X and greater than 2")
-        assert freq_retraining <= len(X), ("freq_retraining should be less than or equal to the length of X")
-        assert lookahead_steps < min_train_steps, ("lookahead_steps should be less than min_train_steps")
-        training_date = min_train_steps
-
-        def _get_slices(
-                training_date:int, freq_retraining:int, 
-                rolling_window_size:int, lenght:int
-            ) -> tuple[slice, slice]:
-            if rolling_window_size:
-                training_slice = slice(max(0, training_date - 1 - rolling_window_size ), training_date - 1 - lookahead_steps)
-                test_slice = slice(training_date, min(training_date + freq_retraining, lenght))
-            else:
-                training_slice = slice(0, training_date - 1 - lookahead_steps)
-                test_slice = slice(training_date, min(training_date + freq_retraining, lenght))
-            return training_slice, test_slice
-
-        while training_date < len(X):
-            training_slice, test_slice = _get_slices(training_date, freq_retraining, rolling_window_size, len(X))
-            X_train, X_test = X[training_slice], X[test_slice]
-            training_date += freq_retraining
-            yield X_train, X_test
-
-    @staticmethod
-    def _fit_predict_static(
-        model: BaseEstimator | object, 
-        X_train: np.ndarray, y_train: np.ndarray, X_test: np.ndarray,
-    ) -> np.ndarray:
-        """Static fit and predict for the multiprocessing"""
-        if (X_train.size == 0) or (y_train.size == 0) or (X_test.size == 0):
-            Warning("Empty training or test data fed into the model. Returning nan values")
-            return np.full((X_test.shape[0], y_train.shape[1]), np.nan)
-        
-        y_hat = model.fit(X_train, y_train).predict(X_test)
-        # del X_train, y_train, X_test
-        return y_hat
-
-    def _fit_predict_ndarray(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
-        
-        assert X.shape[0] == y.shape[0], ("X and y should have the same number of rows")
-        assert np.all(np.isfinite(X)) and np.all(np.isfinite(y)), ("X and y should not have any missing/infinite values")
-
-        X_generator = BaseTimeSeriesModel.window_grouper(X, **self.window_params)
-        y_generator = BaseTimeSeriesModel.window_grouper(y, **self.window_params)
-    
-        tasks = (
-            (_custom_clone_model(self.model), X_train.copy(), y_train.copy(), X_test.copy())
-            for (X_train, X_test), (y_train, _) in zip(X_generator, y_generator)
-        )
-
-        with mp.Pool(BaseTimeSeriesModel.n_jobs) as pool:
-            results = pool.starmap(
-                BaseTimeSeriesModel._fit_predict_static, tasks
-                )
-
-        y_hat = np.concatenate(results)
-        return np.concatenate([np.full(len(y) - len(y_hat), np.nan), y_hat])
-    
-    def _fit_predict_ds(self, X: pd.DataFrame, y: pd.Series) -> pd.Series:
-        assert not X.empty, f"No features for {y.name}"
-        X = _clean_and_reindex(X, y)
-        try:
-            y_hat_values = self._fit_predict_ndarray(X.values, y.values)
-        except Exception as e:
-            print(f'An error occurred during the fit of {y.name}. Returning NaN values. {e}')
-            y_hat_values = np.nan
-        return pd.Series(data=y_hat_values, name=y.name, index=y.index)
-
-    def _fit_predict_df(self, X: pd.DataFrame, y: pd.DataFrame, skipna: bool) -> pd.DataFrame:
-
-        y_hat = pd.DataFrame(index = y.index)
-        for col in y.columns:
-            X_ = X.loc[:, col] if isinstance(X.columns, pd.MultiIndex) else X
-            y_ = y.loc[:, col] if not skipna else y.loc[:, col].dropna()
-            y_hat[col] = self._fit_predict_ds(X_, y_)
-        return y_hat
-    
-        #Can't use multiprocessing because there are child multi process called in the loop
-        # X_tasks = (X.loc[:, col] if isinstance(X.columns, pd.MultiIndex) else X for col in y.columns)
-        # y_tasks = (y.loc[:, col].dropna() if skipna else y.loc[:, col] for col in y.columns)
-        # tasks = zip(X_tasks, y_tasks)
-        # with mp.Pool(BaseTimeSeriesModel.n_jobs) as pool:
-        #     results = pool.starmap(self._fit_predict_ds, tasks)
-        # return pd.DataFrame(results, index = y.index, columns = y.columns)
- 
+            
     def fit(
-        self, X: np.ndarray | pd.DataFrame, y: np.ndarray | pd.Series | pd.DataFrame, skipna: bool = True, 
+        self, X: np.ndarray | pd.DataFrame, y: np.ndarray | pd.Series | pd.DataFrame, 
+        independant_fit:bool = True, skipna: bool = True, 
     ) -> 'BaseTimeSeriesModel':
         
+        kwargs = {
+            "model": self.model,"X": X,"y": y,
+            "window_params": self.window_params,
+            "n_jobs": BaseTimeSeriesModel.n_jobs,
+            }
+
         match (type(X), type(y)):
             case (np.ndarray, np.ndarray):
-                y_hat = self._fit_predict_ndarray(X, y)
+                y_hat = _fit_predict_ndarray(**kwargs)
             case (pd.DataFrame, pd.Series):
-                y_hat = self._fit_predict_ds(X, y if not skipna else y.dropna()).reindex(y.index)
+                y_hat = _fit_predict_ds(**kwargs, skipna=skipna)
             case (pd.DataFrame, pd.DataFrame):
-                y_hat = self._fit_predict_df(X, y, skipna).reindex(y.index)
+                y_hat = _fit_predict_df(**kwargs, independant_fit=independant_fit, skipna=skipna)                
             case _:
                 raise ValueError(
                     f"""Unsupported types: X type {type(X)}, y type {type(y)}. X,y should be in :
                     (np.ndarray, np.ndarray) ; (pd.DataFrame, pd.Series) ; (pd.DataFrame, pd.DataFrame)
                     """
                 )
-        self.y, self.X, self.y_hat = y.copy(), X.copy(), y_hat
+        self.y, self.X, self.y_hat = y.copy(), X.copy(), y_hat.copy()
 
         return self 
     
@@ -147,8 +69,11 @@ class BaseTimeSeriesModel:
         assert hasattr(self, "y_hat"), "Model should be fit before predict"
         return self.y_hat.copy()
     
-    def fit_predict(self, X: np.ndarray | pd.DataFrame, y: np.ndarray | pd.Series | pd.DataFrame, skipna: bool = True) -> np.ndarray | pd.Series | pd.DataFrame:
-        return self.fit(X, y, skipna).predict()
+    def fit_predict(
+            self, X: np.ndarray | pd.DataFrame, y: np.ndarray | pd.Series | pd.DataFrame, 
+            independant_fit:bool = True, skipna: bool = True, 
+        ) -> np.ndarray | pd.Series | pd.DataFrame:
+        return self.fit(X, y, independant_fit, skipna).predict()
     
     @property
     def residual(self) -> np.ndarray | pd.Series | pd.DataFrame:
@@ -165,16 +90,6 @@ class BaseTimeSeriesModel:
         return self.__class__(
             _custom_clone_model(self.model), **self.get_params()
         )
-
-def set_n_jobs(n_jobs: int) -> int:
-    n_jobs = max(1, n_jobs)
-    n_jobs = min(n_jobs, mp.cpu_count() - 2)
-    BaseTimeSeriesModel.n_jobs = n_jobs
-    return n_jobs
-
-# def set_auto_scale(auto_scale: bool = True) -> bool:
-#     BaseTimeSeriesModel.auto_scale = auto_scale
-#     return auto_scale
 
 class RollingModel(BaseTimeSeriesModel):
     def __init__(
--- tskit_learn/utilitaires.py
+++ tskit_learn/utilitaires.py
@@ -1,11 +1,14 @@
-from sklearn.base import BaseEstimator
-from sklearn import clone as sklearn_clone
-from typing import Callable, Any
 import multiprocessing as mp
 import pandas as pd
 import numpy as np
 
-def _custom_clone_model(model: BaseEstimator | object) -> BaseEstimator | object:
+from typing import Generator, Tuple
+
+from sklearn.base import BaseEstimator
+from sklearn import clone as sklearn_clone
+
+def _custom_clone_model(model: BaseEstimator | object) -> BaseEstimator | object: 
+    #Est ce que c'est vraiment nécessaire ?
     try:
         cloned_model = sklearn_clone(model)
         return cloned_model
@@ -18,46 +21,181 @@ def _custom_clone_model(model: BaseEstimator | object) -> BaseEstimator | object
             return cloned_model
         else:
             return model
-            print(e)
+            e
 
 def _clean_and_reindex(X:pd.DataFrame, y:pd.Series = None) -> pd.DataFrame:
     if y is None:
         y = X
     return (
         X
+        .replace([np.inf, -np.inf], np.nan)
         .dropna(how='all', axis=1)
         .dropna(how='all', axis=0)
-        .reindex(y.index)
-        .replace([np.inf, -np.inf], np.nan)
+        .reindex(y.index, method='ffill')
         .ffill()
         .fillna(0)
     )
 
-def expanding_decorator(
-        f:Callable[[pd.DataFrame, Any], pd.Series],
-        n_jobs:int = max(1, mp.cpu_count() - 2),
-    ) -> Callable[[pd.DataFrame, Any], pd.DataFrame]:
-
-    def f_expanding(df:pd.DataFrame, *args, **kwargs) -> pd.DataFrame:
-        ans = pd.DataFrame(index = df.index)
-
-        tasks = ( df[df.index < i].copy() for i in df.index )
-        with mp.Pool(n_jobs) as pool:
-            results = pool.map(f, tasks, args, kwargs)
+def _window_grouper(
+    X: np.ndarray, rolling_window_size: int, freq_retraining: int, min_train_steps: int, lookahead_steps:int
+) -> Generator[Tuple[np.ndarray, np.ndarray], None, None]:
     
-        for i, res in zip(df.index, results):
-            ans.loc[i, :] = res
+    assert 2 < min_train_steps <= len(X), ("min_train_steps should be less than or equal to the length of X and greater than 2")
+    assert freq_retraining <= len(X), ("freq_retraining should be less than or equal to the length of X")
+    assert lookahead_steps < min_train_steps, ("lookahead_steps should be less than min_train_steps")
 
-        return ans
+    def _get_slices() -> tuple[slice, slice]:
+        if rolling_window_size:
+            training_slice = slice(max(0, training_date - 1 - rolling_window_size ), training_date - 1 - lookahead_steps)
+            test_slice = slice(training_date, min(training_date + freq_retraining, len(X)))
+        else:
+            training_slice = slice(0, training_date - 1 - lookahead_steps)
+            test_slice = slice(training_date, min(training_date + freq_retraining, len(X)))
+        return training_slice, test_slice
 
-    return f_expanding
+    for training_date in range(min_train_steps, len(X), freq_retraining):
+        training_slice, test_slice = _get_slices(
+            training_date, freq_retraining, rolling_window_size, len(X), lookahead_steps
+            )
+        X_train, X_test = X[training_slice], X[test_slice]
+        yield X_train, X_test
 
-def expanding_apply(
-        df:pd.DataFrame, 
-        f:Callable[[pd.DataFrame, Any], pd.Series],
-        args:tuple = (),
-        kwargs:dict = {},
-        n_jobs:int = max(1, mp.cpu_count() - 2)
+def _fit_predict_static(
+        model: BaseEstimator | object, X_train: np.ndarray, y_train: np.ndarray, X_test: np.ndarray,
+    ) -> np.ndarray:
+    """Static fit and predict for the multiprocessing"""
+    if (X_train.size == 0) or (y_train.size == 0) or (X_test.size == 0):
+        Warning("Empty training or test data fed into the model. Returning nan values")
+        return np.full((X_test.shape[0], y_train.shape[1]), np.nan)
+    y_hat = model.fit(X_train, y_train).predict(X_test)
+    del X_train, y_train, X_test
+    return y_hat
+
+def _fit_predict_ndarray(
+        model: BaseEstimator | object, X: np.ndarray, y: np.ndarray, window_params: dict, n_jobs: int
+    ) -> np.ndarray:
+    
+    assert X.shape[0] == y.shape[0], ("X and y should have the same number of rows")
+    assert np.all(np.isfinite(X)) and np.all(np.isfinite(y)), ("X and y should not have any missing/infinite values")
+
+    X_generator = _window_grouper(X, **window_params)
+    y_generator = _window_grouper(y, **window_params)
+
+    tasks = (
+        ( _custom_clone_model(model), X_train.copy(), y_train.copy(), X_test.copy() )
+        for (X_train, X_test), (y_train, _) in zip(X_generator, y_generator)
+    )
+
+    with mp.Pool(n_jobs) as pool:
+        results = pool.starmap(
+            _fit_predict_static, tasks
+            )
+
+    y_hat = np.concatenate(results)
+    return np.concatenate([np.full(len(y) - len(y_hat), np.nan), y_hat])
+
+def _fit_predict_ds(
+        model: BaseEstimator | object, X: pd.DataFrame, y: pd.Series, window_params: dict, skipna: bool, n_jobs: int
+        ) -> pd.Series:
+    assert not X.empty, f"No features for {y.name}"
+
+    X = _clean_and_reindex(X, y)    
+    y_ = y if not skipna else y.dropna()
+    try:
+        y_hat_values = _fit_predict_ndarray(model, X.values, y_.values, window_params, n_jobs)
+    except Exception as e:
+        print(f'An error occurred during the fit of {y.name}. Returning NaN values. {e}')
+        y_hat_values = np.nan
+    return pd.Series(
+        data=y_hat_values, name=y.name, index=y.index
+        )
+
+
+##### Unidimensional fit and predict: #####
+##### train each asset separately     #####
+
+def _fit_predict_unidimensional(
+        model, X: pd.DataFrame, y: pd.DataFrame, skipna: bool, window_params: dict, n_jobs: int
     ) -> pd.DataFrame:
 
-    return expanding_decorator(f, n_jobs)(df, *args, **kwargs)
+    y_hat = pd.DataFrame(index = y.index)
+    for col in y.columns:
+        X_ = X.loc[:, col] if isinstance(X.columns, pd.MultiIndex) else X
+        y_ = y.loc[:, col] if not skipna else y.loc[:, col].dropna()
+        y_hat[col] = _fit_predict_ds(model, X_, y_, window_params, skipna, n_jobs)
+
+    return y_hat
+
+
+##### Multidimensional fit and predict:             #####
+##### When all the assets have the same features    #####
+##### and the training is made on the whole dataset #####
+
+def _window_splitter(
+        df:pd.DataFrame, freq_retraining:int, min_periods:int, rolling_window_size:int
+    ) -> Generator[Tuple[pd.DataFrame, pd.DataFrame], None, None]:
+
+    assert 'date' in df.columns
+
+    dates = df['date'].unique().sort_values()
+
+    def _get_slice(i:int) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        # The whole stuff assume that dates are on a regular grid
+        if rolling_window_size:
+            training_slice = (df['date'] < dates[i]) & ( dates[i-rolling_window_size] <= df['date'])
+            test_slice = (dates[i] <= df['date']) & (df['date'] < dates[i+freq_retraining])
+        else:
+            training_slice = (df['date'] < dates[i])
+            test_slice = (dates[i] <= df['date']) & (df['date'] < dates[i+freq_retraining])
+        return training_slice, test_slice
+
+    for i in range(min_periods, len(dates), freq_retraining):
+        training_slice, test_slice = _get_slice(i)
+        yield df[training_slice], df[test_slice]
+
+def _reshaper(X:pd.DataFrame, y:pd.DataFrame) -> pd.DataFrame:
+    X = _clean_and_reindex(X, y)
+    df = X.reorder_levels([1, 0], axis=1)
+    df['target'] = y
+    df = df.stack()
+    df = df[df['target'].notna()]
+    df.index = df.index.set_names(['date', 'asset'])
+    df = df.reset_index().sort_values(['date', 'asset'])
+    return df 
+
+def _fit_predict_shaped(
+        model: BaseEstimator | object, df_train:pd.DataFrame, df_test:pd.DataFrame
+    ) -> pd.DataFrame:
+    X_train = df_train.drop(columns=['date', 'asset', 'target'])
+    y_train = df_train['target']
+    X_test  = df_test.drop(columns=['date', 'asset', 'target'])
+    df_test['pred'] = model.fit(X_train, y_train).predict(X_test).values 
+    df_test = df_test.set_index(['date', 'asset']).unstack()
+    return df_test['pred']
+
+def _fit_predict_multidimensional(
+        model:BaseEstimator | object, X: pd.DataFrame, y: pd.DataFrame, window_params: dict, n_jobs: int
+    ) -> pd.DataFrame:
+
+    assert isinstance(X.columns, pd.MultiIndex), "can't handle non-multidimensional data for multidim fit"
+    assert all(y.columns.equals(X.columns.get_level_values(0).unique())), "X and y should have the same assets"
+    df = _reshaper(X, y)
+    tasks = ( 
+        (_custom_clone_model(model), df_train, df_test) 
+        for df_train, df_test in _window_splitter(df, **window_params)
+    )
+    with mp.Pool(n_jobs) as pool:
+        results = pool.map(_fit_predict_shaped, tasks)
+
+    return pd.concat(results, axis=0).reindex(y.index, method='ffill')
+
+def _fit_predict_df(
+        model:BaseEstimator | object, X: pd.DataFrame, y: pd.DataFrame, 
+        independant_fit:bool, skipna: bool, window_params: dict, n_jobs: int
+    ) -> pd.DataFrame:
+
+    if independant_fit:
+        return _fit_predict_unidimensional(model, X, y, skipna, window_params, n_jobs)
+    else:
+        raise NotImplementedError
+        return _fit_predict_multidimensional(model, X, y, window_params, n_jobs)
